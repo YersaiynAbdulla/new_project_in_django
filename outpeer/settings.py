fac38aa5bab75e8943cd76b3b4856d8afac48281
Yersaiyn <ersaiynabdull2005gmail.com>
2025-04-09 18:40:42 +0500

    hw25

--- outpeer/settings.py
+++ outpeer/settings.py
@@ -77,11 +77,16 @@ WSGI_APPLICATION = 'outpeer.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'outpeer_db'),
+        'USER': os.getenv('DB_USER', 'outpeer_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'outpeer_pass'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
